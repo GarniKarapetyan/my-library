4c02fbae07c3d110a368abf7cab5fbec6eb833d0
Garnik Karapetyan <garnik.karapetyan.2003@inbox.ru>
2022-07-05 20:27:08 +0400

    connecting to MongoDB with djongo

--- mylibrary/settings.py
+++ mylibrary/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mylibrary_app.apps.MylibraryAppConfig'
 ]
 
+# CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -75,8 +79,8 @@ WSGI_APPLICATION = 'mylibrary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'Libraries',
     }
 }
 
--- mylibrary_app/models.py
+++ mylibrary_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class Libraries(models.Model):
+    username = models.CharField(max_length=255, blank=False)
+    password = models.TextField(blank=False)
+    books = ArrayField(models.CharField(max_length=150, blank=True),size=150)
+    price_of_books = models.IntegerField()
+    budget = models.IntegerField()
+    time_update = models.DateTimeField(auto_now=True)
