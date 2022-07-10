8cee6d6b3f249b62350501a08947b12b4c366407
Garnik Karapetyan <garnik.karapetyan.2003@inbox.ru>
2022-07-10 21:14:40 +0400

    Templates base

--- mylibrary_app/views.py
+++ mylibrary_app/views.py
@@ -5,7 +5,8 @@ def log_in_button(request):
     return HttpResponse('<a href=http://127.0.0.1:8000/mylibrary/login/><button type="button", style="background-color:#7CFC00">Log In</button></a>')
 
 def log_in_interface(request):
-    return HttpResponse('<button type="button", style="background-color:#7CFC00">Use your login</button>')
+    return render(request, 'mylibrary_app/index.html')
+    # return HttpResponse('<button type="button", style="background-color:#7CFC00">Use your login</button>')
 
 def user(requset, user_id):
     return HttpResponse(f"<h1>Here will be displayed user {user_id}'s library.</h1>")
